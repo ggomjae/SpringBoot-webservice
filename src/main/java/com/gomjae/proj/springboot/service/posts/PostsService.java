08a94619f87407730f12c5da05f8a1cb528b64fa
ggomjae <ggomjae@naver.com>
2020-01-08 22:34:45 +0900

    5th commit
    1. Mustache 설정, 구현
    2. 게시판 구현 , 등록

--- src/main/java/com/gomjae/proj/springboot/service/posts/PostsService.java
+++ src/main/java/com/gomjae/proj/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.gomjae.proj.springboot.service.posts;
 
 import com.gomjae.proj.springboot.domain.posts.Posts;
 import com.gomjae.proj.springboot.domain.posts.PostsRepository;
+import com.gomjae.proj.springboot.web.dto.PostsListResponseDto;
 import com.gomjae.proj.springboot.web.dto.PostsResponseDto;
 import com.gomjae.proj.springboot.web.dto.PostsSaveRequestDto;
 import com.gomjae.proj.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -37,5 +41,12 @@ public class PostsService {
                 new IllegalArgumentException("해당 사용자가 없습니다. id =" +id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
