a7520dc2e4f0b4f3616a5f9b4602f04dfc81daaf
ggomjae <ggomjae@naver.com>
2020-01-10 18:14:16 +0900

    9th commit
    1.기존 테스트 시큐리티 적용

---
+++ src/main/java/com/gomjae/proj/springboot/cofig/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.gomjae.proj.springboot.cofig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
